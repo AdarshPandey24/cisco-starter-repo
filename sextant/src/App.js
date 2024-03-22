0d33d28b3c8274fffabd4990acac5ea21e4de600
Adarsh Pandey <adarshpandey2342003@gmail.com>
2024-03-22 09:04:56 +0530

    task 3 completed

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import IpAdress from "./IpAdress.js";
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Banner bannerText="Sextant" />
-                <Exhibit name="I'm an exhibit!"></Exhibit>
-                <Exhibit name="I'm also an exhibit!"></Exhibit>
-                <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
+                <Exhibit exhibitTitle="IPv4">
+            <IpAdress ipType="ipv4"></IpAdress>
+          </Exhibit>
+          <Exhibit exhibitTitle="IPv6">
+            <IpAdress ipType="ipv6"></IpAdress>
+          </Exhibit>
             </div>
         );
     }
